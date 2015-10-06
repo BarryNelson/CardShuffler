3758c804cd9e776f509c817633d11f4237064e55
Barry Nelson <barry100001@gmail.com>
2015-10-06 09:36:15 -0700

    Cleanup

--- src/Card.java
+++ src/Card.java
@@ -1,69 +1,66 @@
-
+/**
+ * 
+ * @author Barry Nelson
+ * 
+ */
 public class Card {
 
 	static private int sequence;
-	
-	String[] suitName = {
-			 "Spades"
-			,"Hearts"
-			,"Diamonds"
-			,"Clubs"
-		};
-	String[] shortSuitName = {
-			 "s"
-			,"h"
-			,"d"
-			,"c"
-		};
-	
-	String[] faceName = {
-			  "Two"
-			, "Three"
-			, "Four"
-			, "Five"
-			, "Six"
-			, "Seven"
-			, "Eight"
-			, "Nine"
-			, "Ten"
-			, "Jack"
-			, "Queen"
-			, "King"
-			, "Ace"
-		};
-	
-	String[] shortFaceName = {
-			  "2"
-			, "3"
-			, "4"
-			, "5"
-			, "6"
-			, "7"
-			, "8"
-			, "9"
-			, "10"
-			, "J"
-			, "Q"
-			, "K"
-			, "A"
-		};
-	
+
+	private final static String[] suitName = { "Spades", "Hearts", "Diamonds", "Clubs" };
+	private final static String[] shortSuitName = { "s", "h", "d", "c" };
+	private final static String[] faceName = { "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace" };
+	private final static String[] shortFaceName = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+
 	private int suitValue;
 	private int faceValue;
-	private String cardName;
-
 	private int seqNum;
-	
+
 	public Card(int s, int f) {
 		suitValue = s;
 		faceValue = f;
 		seqNum = Card.sequence++;
 	}
+	
+	public String getSuitName(){
+		return suitName[suitValue];
+	}
+
+	public String getFaceName(){
+		return faceName[faceValue];
+	}
+
+	public String getCardName(){
+		return faceName[faceValue]+" of "+suitName[suitValue];
+	}
+
+	public String getShortSuitName(){
+		return shortSuitName[suitValue];
+	}
+
+	public String getShortFaceName(){
+		return shortFaceName[faceValue];
+	}
+
+	public String getShortCardName(){
+		return shortFaceName[faceValue]+" of "+shortSuitName[suitValue];
+	}
+
+	public int getSuitValue() {
+		return suitValue;
+	}
+
+	public int getFaceValue() {
+		return faceValue;
+	}
+
+	public int getSeqNum() {
+		return seqNum;
+	}
 
 	@Override
 	public String toString() {
-		// return "The "+faceName[faceValue] + " of " + suitName[suitValue];
-		// return shortFaceName[faceValue] + shortSuitName[suitValue];
-		return ""+seqNum;
+		return Integer.toString(seqNum);
 	}
+	
 }
--- src/CardDeck.java
+++ src/CardDeck.java
@@ -1,36 +1,36 @@
 import java.util.Random;
 
 /**
- * CardDeck represents a deck of 52 playing cards
- * cards have the values 0 (ace) through 12 (king)
+ * CardDeck represents a deck of 52 playing cards cards have the values 0 (ace) through 12
+ * (king)
  * 
  * @author Barry Nelson
  *
  */
 public class CardDeck {
-	
 
 	Random random = new Random(1234L);
-	
+
 	private final static int SUITS = 4;
 	private final static int FULLSUIT = 13;
 	private final int deckSize = SUITS * FULLSUIT;
 
 	private int shuffleCount;
-	
+
 	private CNode cards;
-	private CNode lastCardNode;	
-	
+	private CNode lastCardNode;
+
 	/**
 	 * CNode a simple linked list to hold cards
+	 * 
 	 * @author Barry Nelson
 	 *
 	 */
 	class CNode {
-		
+
 		private Card card;
 		private CNode nextCard;
-		
+
 		//
 		public CNode(Card card) {
 			this.card = card;
@@ -44,8 +44,9 @@ public class CardDeck {
 		public void addCardNode(CNode nCard) {
 			nextCard = nCard;
 		}
-		
-		/** cuts CNode into two parts
+
+		/**
+		 * cuts CNode into two parts
 		 * 
 		 * @param cut
 		 * @return remaining CNode after the split
@@ -57,19 +58,18 @@ public class CardDeck {
 				lNode = cNode;
 				cNode = cNode.nextCard;
 			}
-			
+
 			lNode.nextCard = null;
-			
+
 			return cNode;
 		}
 
 		public boolean hasNext() {
-			return nextCard != null ? true: false;
+			return nextCard != null ? true : false;
 		}
-		
+
 		/**
-		 * set a card into the nextCard
-		 * replaces whatever may have been there
+		 * set a card into the nextCard replaces whatever may have been there
 		 * 
 		 * @param nextNode
 		 */
@@ -79,6 +79,7 @@ public class CardDeck {
 
 		/**
 		 * returns the next card
+		 * 
 		 * @return nextCard
 		 */
 		public CNode getNext() {
@@ -93,7 +94,7 @@ public class CardDeck {
 		 */
 		public CNode skip(int count) {
 			CNode temp = this;
-			while(temp.nextCard != null && count-->0){
+			while (temp.nextCard != null && count-- > 0) {
 				temp = temp.nextCard;
 			}
 			return temp;
@@ -106,17 +107,18 @@ public class CardDeck {
 		 */
 		public CNode getLast() {
 			CNode temp = this;
-			while(temp.nextCard != null){
+			while (temp.nextCard != null) {
 				temp = temp.nextCard;
 			}
 			return temp;
 		}
-		
+
 		/**
 		 * attach a CNode list of cards to the last node
+		 * 
 		 * @param newTail
 		 */
-		public void append(CNode newTail){
+		public void append(CNode newTail) {
 			CNode currentEnd = getLast();
 			currentEnd.setNext(newTail);
 		}
@@ -127,27 +129,26 @@ public class CardDeck {
 		 * @return
 		 */
 		public int count() {
-			
+
 			int c = 1;
 			CNode temp = this;
-			
+
 			while (temp.hasNext()) {
 				c++;
 				temp = temp.getNext();
 			}
-			
+
 			return c;
 		}
-	
+
 		@Override
 		public String toString() {
 			String str = card.toString();
-			if (nextCard!=null){
-				str += ", "+nextCard.toString();
+			if (nextCard != null) {
+				str += ", " + nextCard.toString();
 			}
 			return str;
 		}
-
 
 	};
 
@@ -155,12 +156,12 @@ public class CardDeck {
 	 * Initialize CardDeck to the four suits in order, much like from the factory
 	 */
 	public CardDeck() {
-		
+
 		shuffleCount = 0;
-		for (int suit = 0; suit < SUITS; suit++){
-			for (int face=0; face < FULLSUIT; face++){
-				CNode nCard = new CNode(new Card( suit, face ));
-				if (cards==null){
+		for (int suit = 0; suit < SUITS; suit++) {
+			for (int face = 0; face < FULLSUIT; face++) {
+				CNode nCard = new CNode(new Card(suit, face));
+				if (cards == null) {
 					cards = nCard;
 				} else {
 					lastCardNode.addCardNode(nCard);
@@ -169,7 +170,7 @@ public class CardDeck {
 			}
 		}
 	}
-	
+
 	/**
 	 * debug test - verify deck1 and deck2 have the same number of cards
 	 * 
@@ -177,43 +178,37 @@ public class CardDeck {
 	 * @param deck2
 	 */
 	private void checkTotals(CNode deck1, CNode deck2) {
-		
+
 		int x = deck1.count();
-		int y=0;
-		if(deck2!=null){
+		int y = 0;
+		if (deck2 != null) {
 			y = deck2.count();
 		}
-		if(x+y != deckSize){
+		if (x + y != deckSize) {
 			System.out.println("fail");
 			System.exit(0);
 		}
 	}
 
-	@Override
-	public String toString() {
-		return "shuffled " + shuffleCount + " :"+cards;
-	}
-
 	/**
 	 * shuffle the deck
+	 * 
 	 * @param reps
 	 */
 	public void shuffle(int reps) {
-		
-	//	shuffle_striping(random.nextInt(deckSize/5)+(deckSize/6));
-	//	shuffle_striping(random.nextInt(deckSize/3)+(deckSize/6));
+
+		// shuffle_striping(random.nextInt(deckSize/5)+(deckSize/6));
+		// shuffle_striping(random.nextInt(deckSize/3)+(deckSize/6));
 
 		riffleShuffle(reps);
-		
+
 	}
 
 	/**
 	 * stripeShuffle is often used in conjunction to a riffleShuffle when shuffling cards
-	 * method:
-	 *  a group of cards is separated from the top of the deck
-	 *  and placed at the end of the next "stripe" of cards removed from the new top of the deck
-	 *  this procedure repeats until the original deck is exhausted
-	 * 
+	 * method: a group of cards is separated from the top of the deck and placed at the
+	 * end of the next "stripe" of cards removed from the new top of the deck this
+	 * procedure repeats until the original deck is exhausted
 	 * 
 	 * @param stripe
 	 */
@@ -223,114 +218,112 @@ public class CardDeck {
 		for (int i = stripe; i < deckSize;) {
 
 			bDeck = cards.splitNodeAt(stripe);
-			
+
 			if (result == null) {
 				result = cards;
 			} else {
 				cards.append(result);
 				result = cards;
 			}
-			
+
 			cards = bDeck;
-			
+
 			i += stripe;
 		}
 		cards.append(result);
 	}
 
 	/**
-	 * riffleShuffle is the most common form of shuffling cards
-	 * method:
-	 * deck is stripeShuffled
-	 * then
-	 * deck is cut into two decks randomly
-	 * those two decks are merged via shuffleMerge
+	 * riffleShuffle is the most common form of shuffling cards method: deck is
+	 * stripeShuffled then deck is cut into two decks randomly those two decks are merged
+	 * via shuffleMerge
 	 * 
 	 * @param i
 	 */
 	private void riffleShuffle(int i) {
 		int cutSize;
 		for (; i > 0; i--) {
-			
+
 			shuffleCount++;
-			
+
 			do {
 				cutSize = random.nextInt(deckSize);
 			} while (cutSize == 0);
-			
-			float interleaveFactor = (float)cutSize/(float)(deckSize - cutSize);
-			
-			stripeShuffle(random.nextInt(deckSize/5)+(deckSize/6));
-			
+
+			float interleaveFactor = (float) cutSize / (float) (deckSize - cutSize);
+
+			stripeShuffle(random.nextInt(deckSize / 5) + (deckSize / 6));
+
 			CNode bDeck = cards.splitNodeAt(cutSize);
 			shuffleMerge(cards, bDeck, interleaveFactor);
-			
+
 			System.out.println(cards);
-			
+
 		}
 	}
-	
+
 	/**
-	 * merges second CNode deck into first 
-	 * method:
-	 * a random number of cards are "cut" from the top of both decks
-	 * one group goes below the other
-	 * repeat through the remainder of both decks
+	 * merges second CNode deck into first method: a random number of cards are "cut" from
+	 * the top of both decks one group goes below the other repeat through the remainder
+	 * of both decks
 	 * 
 	 * @param deck1
 	 * @param deck2
-	 * @param interleave 
+	 * @param interleave
 	 */
 	private void shuffleMerge(CNode deck1, CNode deck2, float interleaveFactor) {
-		
+
 		CNode tempDeck;
-		
-		//if(deck1.count() < deck2.count()){
-		if(random.nextBoolean() || (deck1.count() < deck2.count())){
+
+		// if(deck1.count() < deck2.count()){
+		if (random.nextBoolean() || (deck1.count() < deck2.count())) {
 			tempDeck = deck1;
 			deck1 = deck2;
 			deck2 = tempDeck;
 			tempDeck = null;
-			interleaveFactor = 1/interleaveFactor;
+			interleaveFactor = 1 / interleaveFactor;
 		}
-		
-		
+
 		int preMergeCount = deck1.count() + deck2.count();
 
 		float interleave = 0.0f;
 		float priorInterleave = 0.0f;
-		
 
 		tempDeck = deck1;
 		do {
 			interleave += interleaveFactor;
-			
-			tempDeck = tempDeck.skip((int)(interleave-priorInterleave));
+
+			tempDeck = tempDeck.skip((int) (interleave - priorInterleave));
 			priorInterleave = interleave;
-			
+
 			tempDeck.setNext(new CNode(deck2, tempDeck.getNext()));
 			tempDeck = tempDeck.skip(2);
-			deck2=deck2.getNext();
-						
+			deck2 = deck2.getNext();
+
 			checkTotals(deck1, deck2);
-			
+
 		} while (tempDeck != null && deck2 != null && tempDeck.hasNext() && deck2.hasNext());
-		
-		while(deck2 != null){
-			tempDeck = tempDeck==null ? deck1.getLast() : tempDeck.getLast();
+
+		while (deck2 != null) {
+			tempDeck = tempDeck == null ? deck1.getLast() : tempDeck.getLast();
 			tempDeck.setNext(new CNode(deck2, tempDeck.getNext()));
 			tempDeck = tempDeck.skip(2);
-			deck2=deck2.getNext();
-		}		
-	
-		int postMergeCount = deck1.count();
-		if (postMergeCount != deckSize){
-			System.out.println("failure: count = "+postMergeCount);
+			deck2 = deck2.getNext();
 		}
-		
+
+		int postMergeCount = deck1.count();
+		if (postMergeCount != deckSize) {
+			System.out.println("failure: count = " + postMergeCount);
+		}
+
 		// store into proper place
 		cards = deck1;
 
+	}
+
+	@Override
+	public String toString() {
+		return "shuffled " + shuffleCount + " :" + cards;
 	}
 
 }
